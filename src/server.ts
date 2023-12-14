daeac6eb412e2242acaa44729da36c25c9c4d880
Archit Jain <architjain798@gmail.com>
2023-12-15 00:14:24 +0530

    add the global error handler

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+// import createError from 'http-errors'
 import app from './app'
 import { Config } from './config'
 import logger from './config/logger'
@@ -5,6 +6,8 @@ import logger from './config/logger'
 const startServer = () => {
     const PORT = Config.PORT
     try {
+        // const err = createError(401, 'Please login to view this page.')
+        // throw err
         // throw new Error('Something went wrong')
         logger.debug('debug message', {})
         app.listen(PORT, () => {
