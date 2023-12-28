eba8804fa0de3ccf366c1d5d3ec679cc25bf8d85
Archit Jain <architjain798@gmail.com>
2023-12-28 20:12:35 +0530

    Add the testcase

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -22,6 +22,46 @@ describe('POST /auth/register', () => {
             // Assert
             expect(response.statusCode).toBe(201)
         })
+
+        it('should return valid json response', async () => {
+            // Arrange
+            const userData = {
+                firstName: 'Archit',
+                lastName: 'Jain',
+                email: 'architjain@gmail.com',
+                password: 'test@123',
+            }
+
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            // Assert
+            expect(
+                (response.headers as Record<string, string>)['content-type'],
+            ).toEqual(expect.stringContaining('json'))
+        })
+
+        it('should persist the user in the database', async () => {
+            // Arrange
+            const userData = {
+                firstName: 'Archit',
+                lastName: 'Jain',
+                email: 'architjain@gmail.com',
+                password: 'test@123',
+            }
+
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            // Assert
+            expect(
+                (response.headers as Record<string, string>)['content-type'],
+            ).toEqual(expect.stringContaining('json'))
+        })
     })
 
     describe('Fields are missing', () => {})
