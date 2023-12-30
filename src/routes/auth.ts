7c085ade0210c450fe693ae1d9c5f5eeee037f21
Archit Jain <architjain798@gmail.com>
2023-12-31 01:04:48 +0530

    Add the unit test case and service and repository layer

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,29 @@
 import express from 'express'
+import { AppDataSource } from '../config/data-source'
 import { AuthController } from '../controllers/AuthController'
+import { User } from '../entity/User'
+import { UserService } from '../services/UserService'
 
 const router = express.Router()
 
-const authController = new AuthController()
+const userRepository = AppDataSource.getRepository(User)
+const userService = new UserService(userRepository)
+const authController = new AuthController(userService)
 
-router.post('/register', (req, res) => authController.register(req, res))
+// router.post('/register', (req, res) => authController.register(req, res))
+
+router.post('/register', (req, res) => {
+    authController
+        .register(req, res)
+        .then(() => {
+            // Do something after successful registration if needed
+            // res.status(200).send('Registration successful')
+        })
+        .catch(() => {
+            // Handle errors
+            // console.error(error)
+            // res.status(500).send('Internal Server Error')
+        })
+})
 
 export default router
