77f1b5ec864b019bcd50cf8f404392a63ae41269
Archit Jain <architjain798@gmail.com>
2024-05-29 00:19:30 +0530

    Add the test case for create user

--- tests/users/create.spec.ts
+++ tests/users/create.spec.ts
@@ -58,10 +58,59 @@ describe('POST /users', () => {
 
             //Assert
             expect(users).toHaveLength(1)
-            expect(users[0].role).toBe(Roles.MANAGER)
             expect(users[0].email).toBe(userData.email)
+            // expect(users[0].role).toBe(Roles.MANAGER)
         })
 
-        it.todo('should create a manager user')
+        it('should create a manager user', async () => {
+            // Register the user
+            const userData = {
+                firstName: 'Archit',
+                lastName: 'Jain',
+                email: 'architjain@gmail.com',
+                password: 'test@123',
+                tenantId: 1,
+            }
+
+            const adminToken = jwks.token({
+                sub: '1',
+                role: Roles.ADMIN,
+            })
+
+            await request(app)
+                .post('/users')
+                .set('Cookie', [`accessToken=${adminToken};`])
+                .send(userData)
+
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+
+            //Assert
+            expect(users).toHaveLength(1)
+            expect(users[0].role).toBe(Roles.MANAGER)
+        })
+        it('should return 403 if non admin user tries to create a user', async () => {
+            // Register the user
+            const userData = {
+                firstName: 'Archit',
+                lastName: 'Jain',
+                email: 'architjain@gmail.com',
+                password: 'test@123',
+                tenantId: 1,
+            }
+
+            const customerToken = jwks.token({
+                sub: '1',
+                role: Roles.CUSTOMER,
+            })
+
+            const response = await request(app)
+                .post('/users')
+                .set('Cookie', [`accessToken=${customerToken};`])
+                .send(userData)
+
+            //Assert
+            expect(response.statusCode).toBe(403)
+        })
     })
 })
